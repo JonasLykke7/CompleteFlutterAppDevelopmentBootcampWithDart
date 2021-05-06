ebd0036a36ce0bda9a4bde3077c357d73a60ac23
Jonas Lykke <jonasthlykke@gmail.com>
2021-05-06 14:15:41 +0200

    24

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point for all our Flutter apps.
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}

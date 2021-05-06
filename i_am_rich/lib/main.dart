3733a1f9fc64d9e1ee1e5bf3200df78244bedb8a
Jonas Lykke <jonasthlykke@gmail.com>
2021-05-06 14:47:37 +0200

    26

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -9,8 +9,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
       ),
     );
   }

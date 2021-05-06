ea80c164d7133df5f0758f1ce76c14b7026cd5b6
Jonas Lykke <jonasthlykke@gmail.com>
2021-05-06 21:53:16 +0200

    44

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -12,57 +12,22 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            // children: <Widget>[
-            //   Container(
-            //     width: 30.0,
-            //     color: Colors.white,
-            //     child: Text('Container 1'),
-            //   ),
-            //   SizedBox(
-            //     width: 30.0,
-            //   ),
-            //   Container(
-            //     color: Colors.blue,
-            //     child: Text('Container 2'),
-            //   ),
-            //   Container(
-            //     color: Colors.red,
-            //     child: Text('Container 3'),
-            //   ),
-            // ],
-
-            children: <Widget>[
-              Container(
-                color: Colors.red,
-                height: double.infinity,
-                width: 100.0,
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/jonas_lykke.png'),
+            ),
+            Text(
+              'Jonas Lykke',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    color: Colors.yellow,
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                  Container(
-                    color: Colors.green,
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.blue,
-                height: double.infinity,
-                width: 100.0,
-              ),
-            ],
-          ),
-        ),
+            ),
+          ],
+        )),
       ),
     );
   }

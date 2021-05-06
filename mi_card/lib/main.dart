f3c77b449b188438acad87d088799e02d735233c
Jonas Lykke <jonasthlykke@gmail.com>
2021-05-06 21:40:29 +0200

    43

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -12,27 +12,57 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: 30.0,
-              color: Colors.white,
-              child: Text('Container 1'),
-            ),
-            SizedBox(
-              width: 30.0,
-            ),
-            Container(
-              color: Colors.blue,
-              child: Text('Container 2'),
-            ),
-            Container(
-              color: Colors.red,
-              child: Text('Container 3'),
-            ),
-          ],
-        )),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // children: <Widget>[
+            //   Container(
+            //     width: 30.0,
+            //     color: Colors.white,
+            //     child: Text('Container 1'),
+            //   ),
+            //   SizedBox(
+            //     width: 30.0,
+            //   ),
+            //   Container(
+            //     color: Colors.blue,
+            //     child: Text('Container 2'),
+            //   ),
+            //   Container(
+            //     color: Colors.red,
+            //     child: Text('Container 3'),
+            //   ),
+            // ],
+
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                height: double.infinity,
+                width: 100.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    color: Colors.yellow,
+                    height: 100.0,
+                    width: 100.0,
+                  ),
+                  Container(
+                    color: Colors.green,
+                    height: 100.0,
+                    width: 100.0,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                height: double.infinity,
+                width: 100.0,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

ab478e2ae0a026a6c27a037e642383f1f426a348
Jonas Lykke <jonasthlykke@gmail.com>
2021-05-07 11:58:28 +0200

    60-61
    Challenge Solution

--- dicee/lib/main.dart
+++ dicee/lib/main.dart
@@ -34,6 +34,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = Random().nextInt(6) + 1;
   int rightDiceNumber = Random().nextInt(6) + 1;
 
+  void rollDices() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -42,21 +49,13 @@ class _DicePageState extends State<DicePage> {
           Expanded(
             child: TextButton(
               child: Image.asset('images/dice$leftDiceNumber.png'),
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                });
-              },
+              onPressed: rollDices,
             ),
           ),
           Expanded(
             child: TextButton(
               child: Image.asset('images/dice$rightDiceNumber.png'),
-              onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                });
-              },
+              onPressed: rollDices,
             ),
           ),
         ],

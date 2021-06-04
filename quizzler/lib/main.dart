f18707ab39d9ad127f990e6f6eb70c84bb5067ae
Jonas Lykke <jonasthlykke@gmail.com>
2021-06-04 14:38:24 +0200

    91
    Checking User Answers

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -32,6 +32,12 @@ class _QuizPageState extends State<QuizPage> {
     'A slug\'s blood is green.',
   ];
 
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+
   int questionNumber = 0;
 
   @override
@@ -71,6 +77,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer == true) {
+                  print('User got it right!');
+                } else {
+                  print('User got it wrong!');
+                }
+
                 setState(() {
                   questionNumber++;
                 });
@@ -92,6 +106,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = answers[questionNumber];
+
+                if (correctAnswer == false) {
+                  print('User got it right!');
+                } else {
+                  print('User got it wrong!');
+                }
+
                 setState(() {
                   questionNumber++;
                 });

2c3ee2442b3a26fd4b1ba98b46cfba998e5fc5ec
Jonas Lykke <jl@iospect.dk>
2021-06-08 13:55:01 +0200

    129
    [Dart] Functions as First Order Objects

--- bmi-calculator/lib/reusable_card.dart
+++ bmi-calculator/lib/reusable_card.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
+  final Function onPress;
   final Color color;
   final Widget cardChild;
 
   ReusableCard({
+    this.onPress,
     @required this.color,
     this.cardChild,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: color,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: color,
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }

a2c0f1ec32814eb5045378d0ec550f7b47d415e2
Jonas Lykke <jonasthlykke@gmail.com>
2021-06-16 14:02:07 +0200

    144
    Async/Await Challenge

--- Clima/lib/screens/loading_screen.dart
+++ Clima/lib/screens/loading_screen.dart
@@ -1,6 +1,5 @@
+import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-
-import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -16,10 +15,12 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    Location location = Location();
 
-    print(position);
+    await location.getCurrentLocation();
+
+    print(location.latitude);
+    print(location.longitude);
   }
 
   @override

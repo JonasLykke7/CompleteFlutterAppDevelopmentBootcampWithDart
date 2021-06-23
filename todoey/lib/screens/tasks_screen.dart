1a2136759f7785ccd4934ec8b6d7db949067e7db
Jonas Lykke <jonasthlykke@gmail.com>
2021-06-23 14:58:37 +0200

    197
    The ListView Challenge

--- todoey/lib/screens/tasks_screen.dart
+++ todoey/lib/screens/tasks_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:todoey/widgets/tasks_list.dart';
+
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -55,6 +57,9 @@ class TasksScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20.0,
+              ),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -62,6 +67,7 @@ class TasksScreen extends StatelessWidget {
                   topRight: Radius.circular(20.0),
                 ),
               ),
+              child: TasksList(),
             ),
           ),
         ],
